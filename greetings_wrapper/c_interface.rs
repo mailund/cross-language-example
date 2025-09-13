76ce9e99387afb88e70b5afd2705ff9f30a325b5
Thomas Mailund <tm@kvantify.dk>
2025-09-13 14:40:15 +0200

    Revert "Hacking up a new example"
    This reverts commit c25d4239750a517bd5dfeab3af9146e1098d2f3c.

---
+++ greetings_wrapper/c_interface.rs
@@ -0,0 +1,21 @@
+use greetings_lib::give_greeting;
+
+#[no_mangle]
+pub extern "C" fn get_greeting() -> *const std::os::raw::c_char {
+    std::ffi::CString::new(give_greeting()).unwrap().into_raw()
+}
+
+/// Frees a C string previously created by Rust.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences a raw pointer.
+/// The pointer must have been previously returned by a call to get_greeting().
+/// After this call, the pointer is invalid and should not be used.
+#[no_mangle]
+pub unsafe extern "C" fn free_greeting(ptr: *mut std::os::raw::c_char) {
+    if !ptr.is_null() {
+        let _ = std::ffi::CString::from_raw(ptr as *mut _);
+        // CString will be dropped here, freeing the memory
+    }
+}
---
+++ hello/Program.cs
@@ -0,0 +1,5 @@
+using Greetings;
+using System;
+
+string message = GreetingService.GetGreeting();
+Console.WriteLine(message);
